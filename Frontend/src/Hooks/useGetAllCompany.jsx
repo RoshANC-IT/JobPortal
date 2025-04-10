0f268bbf96ed47aa5c0ecfa1b76d39448ae19fa9
Roshan Chauhan <roshanranjput92@gmail.com>
2025-04-10 09:47:01 +0530

    Update useGetAllCompany.jsx

--- Frontend/src/Hooks/useGetAllCompany.jsx
+++ Frontend/src/Hooks/useGetAllCompany.jsx
@@ -1,32 +1,3 @@
-// import { useDispatch } from 'react-redux';
-// import { setcompanies, setsingleCompany } from '@/components/redux/companySlice';
-// import { COMPANY_API_END_POINT } from '@/utils/constant';
-// import axios from 'axios';
-// import { useEffect } from 'react';
-
-// function useGetAllCompany(companyId) {
-//   const dispatch = useDispatch();
-//   useEffect(() => {
-//     const fetchCompanies = async () => {
-//       try {
-//         const res = await axios.get(`${COMPANY_API_END_POINT}/get`, { withCredentials: true });
-//         if (res.data.success) {
-//           dispatch(setcompanies(res.data.companies)); // Dispatch company data to Redux store
-//         }
-//       } catch (error) {
-//         console.error(error); // Handle error gracefully
-//       }
-//     };
-//     fetchCompanies();
-//   }, []); // Only run the effect when companyId changes
-// }
-
-// export default useGetAllCompany;
-
-
-
-
-
 import { useDispatch } from 'react-redux';
 import { setcompanies } from '@/components/redux/companySlice';
 import { COMPANY_API_END_POINT } from '@/utils/constant';
